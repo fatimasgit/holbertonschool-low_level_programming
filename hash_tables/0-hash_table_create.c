242880483ef38f7ed8de75021854d108b80ae69d
fatimasgit <fatimamedova@inbox.ru>
2023-12-30 23:49:10 -0800

    asas

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -2,12 +2,11 @@
 /**
   * hash_table_create - creates a hash table
   * @size: size of the array of the hash table
-  *
-  * Return: Pointer to the newly created hash table or Null if something goes wrong
+  * Return: Pointer to the newly crea
   */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *ht;
+	hash_table_t *ht;
 
 	if (size == 0)
 		return (NULL);
